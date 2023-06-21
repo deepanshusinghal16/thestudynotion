8d965c87f83478869c6d1c2ba55afd73af998781
Deepanshu Singhal <deepanshusinghal2003@gmail.com>
2023-06-21 10:36:41 +0530

    Awesome ProgressBar now in couruse Creation Step

--- src/components/core/Dashboard/AddCourse/RenderSteps.jsx
+++ src/components/core/Dashboard/AddCourse/RenderSteps.jsx
@@ -4,63 +4,22 @@ import { FaCheck } from "react-icons/fa";
 import CourseInformationForm from './CouseInformation/CourseInformationForm';
 import CourseBuilderForm from './CourseBuilder/CourseBuilderForm';
 import PublishCourseForm from './PublishCourse/PublishCourseForm';
+import StepProgressBar from './ProgressBar';
 
 const RenderSteps = () => {
     const { step } = useSelector((state) => state.course);
 
-    const steps = [
-        {
-            id: 1,
-            title: "Course Info",
-        },
-        {
-            id: 2,
-            title: "Build",
-        },
-        {
-            id: 3,
-            title: "Publish",
-        },
-    ];
+
 
 
 
 
     return (
-        <div className='w-full '>
+        <div className='w-full px-2'>
 
-            <div className='grid grid-cols-3 place-content-center place-items-center  w-full gap-10'>
-                {
-                    steps.map((item) => (
-                        <>
-                            <div className='flex flex-col relative gap-2'>
-                                <div className={`${step === item.id ? "text-yellow-50 border-yellow-50 border-2  " : step <= item.id ? "border-richblack-700 bg-richblack-800 text-richblack-300" : "border-yellow-50 border-2"} w-fit p-2 rounded-full  backdrop-blur-2xl aspect-square flex item justify-center mx-auto z-[1]`} key={item.id} >
-                                    {
-                                        step > item.id ? (<FaCheck className='text-xl text-yellow-5  ' />) : (item.id)
-                                    }
-                                </div>
-                                <div>
-                                    <p className={`${item.id <= step ? "text-richblack-100" : "text-richblack-500"}`}>{item.title}</p>
-                                </div>
-
-
-                                <div className='absolute top-[30%] right-[10%] translate-x-[100%]'>
-                                    {
-                                        item.id !== steps.length && (
-                                            <div className={`w-28  h-[1px]  border-dashed border-[1px] 
-                                            ${item.id < step ? "border-yellow-25 " : "border-richblack-25"}`}></div>
-                                        )
-                                    }
-                                </div>
-
-                            </div>
-
-
-                        </>
-                    ))
-                }
+            <div className='mx-auto my-4 pb-6 px-1 sm:px-3 rounded-lg  w-full'>
+                <StepProgressBar />
             </div>
-
 
             {/* here start the course form */}
             <div className='mx-auto my-4 px-1 sm:px-3 rounded-lg bg-richblack-800 w-full'>
