ab1c928c154e9a4d60dc4a08e3cf10e0cee6bbcf
Deepanshu Singhal <deepanshusinghal2003@gmail.com>
2023-07-03 18:15:21 +0530

    Template of email

--- server/mail/templates/courseEnrollmentEmail.js
+++ server/mail/templates/courseEnrollmentEmail.js
@@ -76,7 +76,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
                     are excited to have you as a participant!</p>
                 <p>Please log in to your learning dashboard to access the course materials and start your learning journey.
                 </p>
-                <a class="cta" href="https://studynotion-frontend.onrender.com/dashboard">Go to Dashboard</a>
+                <a class="cta" href="https://studynotion-frontend.onrender.com">Go to Dashboard</a>
             </div>
             <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
                     href="mailto:deepanshusinghal2003@gmail.com">info@studynotion.com</a>. We are here to help!</div>
